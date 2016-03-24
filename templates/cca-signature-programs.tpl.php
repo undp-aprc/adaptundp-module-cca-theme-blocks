6a439fcbd08c8554e6afd6edae68cbd0754da177
Matt Clark <matthew.clark@undp.org>
2016-03-24 13:49:26 +1100

    Improved aliasing of taxonomy terms in menu

--- templates/cca-signature-programs.tpl.php
+++ templates/cca-signature-programs.tpl.php
@@ -2,7 +2,7 @@
     <?php foreach($variables['elements']['#items'] as $item): ?>
         <?php $path_alias = url('taxonomy/term/'.$item['tid']); ?>
         <div class="col-sm-2">
-            <a href="/taxonomy/term/<?php print $path_alias ?>">
+            <a href="<?php print $path_alias ?>">
                 <span class="hidden-xs"><?php print(render($item['img'])); ?></span>
                 <h5><?php print $item['title']; ?></h5>
             </a>
