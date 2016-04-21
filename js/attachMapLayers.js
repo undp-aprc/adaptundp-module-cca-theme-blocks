1d176e9d46d99883a2c857db439ceaceb171784c
Matt Clark <matthew.clark@undp.org>
2016-04-21 15:55:53 +1000

    Updates to theme modules

--- js/attachMapLayers.js
+++ js/attachMapLayers.js
@@ -1,11 +1,31 @@
 (function ($) {
 	Drupal.behaviors.cca_theme_blocks_map_layers = {
 		attach: function (context, settings) {
-            $('.impact-story-select-icon', context).bind('click', function() {
+
+            $('.bef-checkboxes', context).find('input[type=checkbox]').each(function() {
+                if($(this).prop('checked')) {
+                    var id = $(this).prop('value');
+                    var targetId = $('#impact-story-select-icon-'+id);
+                    if (targetId.hasClass('inactive')) {
+                        targetId.removeClass('inactive', context);
+                    } else {
+                        targetId.addClass('inactive', context);
+                    }
+                }
+            });
+
+
+            $('.impact-story-select-icon').on('click', function() {
                 $(this).toggleClass('inactive');
-                $('select#edit-field-impact-theme-target-id').val($(this).attr('data-id'));
-                $('div.view-filters form').submit();
-            })
+                var dataId = $(this).data('id');
+                var targetId = $('#edit-field-impact-theme-target-id-'+dataId);
+                if (targetId.prop('checked')) {
+                    targetId.removeProp('checked', context);
+                } else {
+                    targetId.prop('checked','checked', context);
+                }
+                $('.ctools-auto-submit-click').click();
+            });
 
         }
 	}
