b48895982d97f8371e2d207ab76a6aea44246443
Matt Clark <matthew.clark@undp.org>
2016-05-16 14:36:17 +1000

    Updated logo lineup

--- templates/nap-footer-gsp.tpl.php
+++ templates/nap-footer-gsp.tpl.php
@@ -1,24 +1,26 @@
 <div class="footer">
-    <div class="logo-heading">
-        Proudly Supported By
-    </div>
+    <div class="header"><span>Proudly Supported By</span></div>
     <div class="clearfix"></div>
     <div id="logo-list">
-        <div class="top">
-            <div class="logo-container undp"><img src="/sites/all/themes/custom/nap/assets/img/logos/undp.svg" /></div>
-            <div class="logo-container unep"><img src="/sites/all/themes/custom/nap/assets/img/logos/unep.svg" /></div>
-            <div class="logo-container gef"><img src="/sites/all/themes/custom/nap/assets/img/logos/gef.svg" /></div>
-            <div class="logo-container unfccc"><img src="/sites/all/themes/custom/nap/assets/img/logos/unfccc.svg" /></div>
+        <div class="container">
+            <div class="logo-row top-row">
+                <div class="logo undp portrait large"><img src="/sites/all/themes/custom/nap/assets/img/logos/undp.svg" /></div>
+                <div class="logo unep square large"><img src="/sites/all/themes/custom/nap/assets/img/logos/unep.svg" /></div>
+                <div class="logo gef square large"><img src="/sites/all/themes/custom/nap/assets/img/logos/gef.svg" /></div>
+                <div class="logo unfccc landscape large"><img src="/sites/all/themes/custom/nap/assets/img/logos/unfccc.svg" /></div>
+            </div>
+            <div class="logo-row middle-row">
+                <div class="logo fao square"><img src="/sites/all/themes/custom/nap/assets/img/logos/fao.svg" /></div>
+                <div class="logo giz landscape"><img src="/sites/all/themes/custom/nap/assets/img/logos/giz.svg" /></div>
+                <div class="logo global-water-partnership landscape"><img src="/sites/all/themes/custom/nap/assets/img/logos/global-water-partnership.svg" /></div>
+                <div class="logo unisdr landscape"><img src="/sites/all/themes/custom/nap/assets/img/logos/unisdr.svg" /></div>
+            </div>
+            <div class="logo-row bottom-row">
+                <div class="logo ifad square"><img src="/sites/all/themes/custom/nap/assets/img/logos/ifad.svg" /></div>
+                <div class="logo who landscape"><img src="/sites/all/themes/custom/nap/assets/img/logos/who.svg" /></div>
+                <div class="logo un-habitat landscape"><img src="/sites/all/themes/custom/nap/assets/img/logos/un-habitat.jpg" /></div>
+                <div class="logo wmo square"><img src="/sites/all/themes/custom/nap/assets/img/logos/wmo.png" /></div>
+            </div>
         </div>
-        <div class="clearfix"></div>
-        <div class="bottom">
-            <div class="logo-container fao"><img src="/sites/all/themes/custom/nap/assets/img/logos/fao.svg" /></div>
-            <div class="logo-container giz"><img src="/sites/all/themes/custom/nap/assets/img/logos/giz.svg" /></div>
-            <div class="logo-container global-water-partnership"><img src="/sites/all/themes/custom/nap/assets/img/logos/global-water-partnership.svg" /></div>
-            <div class="logo-container unisdr"><img src="/sites/all/themes/custom/nap/assets/img/logos/unisdr.svg" /></div>
-            <div class="logo-container ifad"><img src="/sites/all/themes/custom/nap/assets/img/logos/ifad.svg" /></div>
-            <div class="logo-container who"><img src="/sites/all/themes/custom/nap/assets/img/logos/who.svg" /></div>
-        </div>
-        <div class="clearfix"></div>
     </div>
 </div>
