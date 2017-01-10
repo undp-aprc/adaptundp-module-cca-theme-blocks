5748e7eef52f7d0ba61d6d14530d5eef64dbb718
Matt Clark <matthew.clark@undp.org>
2017-01-11 07:21:14 +1100

    Updated theme blocks

--- templates/cca-media-menu.tpl.php
+++ templates/cca-media-menu.tpl.php
@@ -3,13 +3,25 @@
         <div class="col-sm-2">
             <a href="/news">
                 <img class="hidden-xs" src="/<?php print $theme_path; ?>/img/ux-icons/icon-media-news.svg" />
-                <h5>News</h5>
+                <h5>Adaptation Portfolio News</h5>
             </a>
         </div>
         <div class="col-sm-2">
             <a href="/videos">
                 <img class="hidden-xs" src="/<?php print $theme_path; ?>/img/ux-icons/icon-media-video.svg" />
                 <h5>Videos</h5>
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <a href="/exposure-stories">
+                <img class="hidden-xs" src="/<?php print $theme_path; ?>/img/ux-icons/icon-exposure.svg" />
+                <h5>Exposure Stories</h5>
+            </a>
+        </div>
+        <div class="col-sm-2">
+            <a href="/resources/news-feed">
+                <img class="hidden-xs" src="/<?php print $theme_path; ?>/img/ux-icons/icon-media-in-the-news.svg" />
+                <h5>In the News</h5>
             </a>
         </div>
 </ul>
